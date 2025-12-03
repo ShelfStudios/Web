1a09ea74c29d0866fb21a49b5a972907abd0c783
Cadan <shelfstudios@gmail.com>
2025-12-03 01:46:19 +0000

    update

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+﻿import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../components/Logo';
 
